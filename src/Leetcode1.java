fbdb53114465482136d1a0c1caeb75ced9424c60
HaoyuanJi <haoyuanji@gmail.com>
2014-03-09 16:25:22 -0400

    creat a project

---
+++ src/Leetcode1.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Stack;
+public class Leetcode1 {	
+	 public static String reverseWords(String s) {
+	        
+		 Stack<String> tempsen = new Stack<String>();
+	        char[] temp = s.toCharArray();
+	        ArrayList<Character> word = new ArrayList<Character>();
+	        StringBuffer sb = new StringBuffer();
+	        int countspace = 0;
+	        boolean start = false;
+	        
+	        for(int i = 0; i< temp.length; i++)
+	        {
+	           char letter = temp[i];
+	           if(letter==' ')
+	           {
+	           	   countspace++;
+	               if(countspace > 1 || start == false)
+	               {
+                     continue;
+	               }
+	               else{
+	        	      for (char cc:word)
+	        	      {
+	        		    sb.append(cc);
+	        	      }
+	                  String tempword = sb.toString();
+	                  tempsen.push(tempword);
+	                  sb = new StringBuffer();
+	                  word.clear();
+	               }
+	           }
+	           else
+	           {
+	           	   start = true;
+	           	   countspace = 0;
+	               word.add(letter);
+	           }
+	        }
+	   if(!word.isEmpty())
+	   {
+  	   for (char cc:word)
+  	   {
+  		   sb.append(cc);
+  	   }
+        String tempword = sb.toString();
+        tempsen.push(tempword);
+	   }    
+	        StringBuffer sb2 = new StringBuffer();
+	        
+	        while(!tempsen.empty())
+	        {
+	            String newword = tempsen.pop();
+	            sb2.append(newword);
+	            sb2.append(' ');
+	        }
+	        
+	        if (sb2.length()>0)
+	        {
+	         sb2.deleteCharAt(sb2.length()-1);
+	        }
+	        return sb2.toString();
+	        
+	    }
+	 public static void main(String args[])
+	 {
+		 String input = "jijoji  uy rttnnnp juipi o hyo drdtdrtdt ftyu";
+		 String output = reverseWords(input);
+		 System.out.print(output);
+		 
+	 }
+
+}
