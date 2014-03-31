e8efa2eb4dc1b02475500341638799826288ffaf
HaoyuanJi <haoyuanji@gmail.com>
2014-03-31 16:39:54 -0400

    For Leetcode 7 and 8 - non-recursive tree traversal

--- src/Leetcode7.java
+++ src/Leetcode7.java
@@ -1,4 +1,65 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 
 public class Leetcode7 {
 
+	  public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	  
+	  public ArrayList<Integer> stackpreorderTraversal(TreeNode root) {
+		    ArrayList<Integer> result = new ArrayList<Integer>();
+		    Stack<TreeNode> working = new Stack<TreeNode>();
+		    
+		    if(root != null)
+		    {
+		      working.push(root);		
+		      
+		      while(!working.empty())
+		      {		
+		    	 TreeNode current = working.pop();
+		    	 result.add(current.val);
+		    	 //System.out.println(current.val);
+		    	 
+	    	     if (current.right != null)
+	    	     {
+	    	    	 working.push(current.right);
+	    	     }
+	    	     
+	    	     if(current.left != null)
+	    	     {
+	    	    	 working.push(current.left);
+	    	     }
+		      }
+		    }
+		    
+		   		  
+	        return result;
+	    }
+	 
+	  public static void main(String args[])
+	  {
+		  TreeNode root = new Leetcode7().new TreeNode(1);
+		  TreeNode node1 = new Leetcode7().new TreeNode(2);
+		  root.left = node1;
+		  TreeNode node3 = new Leetcode7().new TreeNode(6);
+		  root.right = node3;
+		  TreeNode node2 = new Leetcode7().new TreeNode(3);
+		  node1.right = node2;
+		  
+		  
+		  ArrayList<Integer> result = new ArrayList<Integer>();
+		  		  
+          result = new Leetcode7().stackpreorderTraversal(root);
+          
+          for (Integer show:result)
+          {
+        	  System.out.println(show);
+          }
+		  
+	  }
 }
