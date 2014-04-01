778de8a49a81a7913d8bfa1d35d6a00dd95d9c4d
HaoyuanJi <haoyuanji@gmail.com>
2014-04-01 18:57:14 -0400

    max points on line

--- src/Leetcode3.java
+++ src/Leetcode3.java
@@ -1,4 +1,23 @@
 
 public class Leetcode3 {
+	
+	class Point {
+		      int x;
+		      int y;
+		      Point() { x = 0; y = 0; }
+		      Point(int a, int b) { x = a; y = b; }
+		  }
 
+    public int maxPoints(Point[] points) {
+        
+        
+        return 0;
+    }
+    
+    public static void main(String args[])
+    {
+    	
+    	
+    	
+    }
 }
