6fb78f7aa28c7eb4623d9790fc3cae38be459749
HaoyuanJi <haoyuanji@gmail.com>
2014-05-06 14:53:46 -0400

    just a test

---
+++ src/Leetcodea11.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+
+
+public class Leetcodea11 {
+	
+	
+	public static void main(String args[])
+	{
+		String s = "Iamagreatking";
+		Set<String> dict = new Set<String>();
+		
+		
+		
+		
+	}
+
+}
